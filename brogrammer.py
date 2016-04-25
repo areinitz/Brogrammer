5db718ea2740ea04fda7407f6e96640af6ef839b
Aaron Reinitz <aaron.reinitz@gmail.com>
2016-04-25 15:46:29 -0400

    Updated to add while loop

--- brogrammer.py
+++ brogrammer.py
@@ -1,6 +1,4 @@
 print "Welcome to Brogrammer"
-print "What part of your body, would you like to work on?"
-print "Enter Arms, Legs, Back, or Cardio:"
 
 ezR = 10
 medR = 15
@@ -25,52 +23,6 @@ if work == "Arms" or work == "arms":
 	elif effort == "CRAY" or effort == "cray":
 		print "Get wild on %d sets of %d curls, %d sets of %d dips, and 50 pushups." % (crayS, crayR, crayS, crayR)
 	else:
-		print "%s sounds like a gnary typo, bro." % effort
+		print "%s sounds like a gnary typo, try again." % wo
 
-if work == "Legs" or work == "legs":
-	print "Nice, urryday's a leg day."
-	print "OK, how hard are we going here, easy, medium, or CRAY?"
-
-	effort = raw_input("> ")
-
-	if effort == "easy" or effort == "Easy":
-		print "Do %d sets of %d leglifts, %d sets of %d hamstrings." % (ezS, ezR, ezS, ezR)
-	elif effort == "medium" or effort == "Medium":
-		print "Do %d sets of %d leglifts, %d sets of %d hamstrings." % (medS, medR, medS, medR)
-	elif effort == "CRAY" or effort == "cray":
-		print "Push through %d sets of %d leglifts, %d sets of %d hamstrings, and 50 lunges." % (crayS, crayR, crayS, crayR)
-	else:
-		print "%s sounds like a gnary typo, bro." % effort
-
-if work == "Back" or work == "back":
-	print "What's that they say about the straw and the camel?"
-	print "How hard are we going here, easy, medium, or CRAY?"
-
-	effort = raw_input("> ")
-
-	if effort == "easy" or effort == "Easy":
-		print "Do %d sets of %d rows, %d sets of %d pullups." % (ezS, ezR, ezS, ezR)
-	elif effort == "medium" or effort == "Medium":
-		print "Do %d sets of %d rows, %d sets of %d pullups." % (medS, medR, medS, medR)
-	elif effort == "CRAY" or effort == "cray":
-		print "Push through %d sets of %d rows, %d sets of %d pullups, and 100 reverse flys." % (crayS, crayR, crayS, crayR)
-	else:
-		print "%s sounds like a gnary typo, bro." % effort
-
-if work == "Cardio" or work == "cardio":
-	print "Run Forrest!"
-	print "Are you looking for something easy, medium, or CRAY?"
-
-	effort = raw_input("> ")
-
-	if effort == "easy" or effort == "Easy":
-		print "Do 20 minutes on the treadmill at a 2% incline."
-	elif effort == "medium" or effort == "Medium":
-		print "Do 30 minutes on the treadmill at a 3% incline." 
-	elif effort == "CRAY" or effort == "cray":
-		print "Try out a 5 mile run alternating between 3% and 5% incline every quarter mile."
-	else:
-		print "%s sounds like a gnary typo, bro." % effort
-
-
-
+print "OK good effort bro, get a shake."
